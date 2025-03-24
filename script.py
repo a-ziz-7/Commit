d65abd2be47067f9642ff0ec102e73090338cf07
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2025-03-24 16:21:46 -0400

    first commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import os
+from random import randint
+
+for i in range(0, 25):
+    x = randint(15, 35)
+    for j in range(x):
+        d = str(i) + ' days ago'
+        with open('file.txt', 'a') as file:
+            file.write(d+'\n')
+        os.system('git add .')
+        os.system('git commit --date="' + d + '" -m "commit"')
+
+os.system('git push -u origin main')
